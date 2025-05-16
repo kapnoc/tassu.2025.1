b24a7ea2a86aaa5cfc5349bd3530666ede08d5d6
Tanguy Gérôme <tanguy@juustodiilerit.fi>
2025-05-16 12:22:10 +0300

    Add LÖHÖILY SEIS! and compress also .JPG extensions

--- compress_assets.sh
+++ compress_assets.sh
@@ -1,5 +1,5 @@
 
-for file in `find ./assets/*.jpg -type f -size +512k`
+for file in `find ./assets/*.{jpg,JPG,jpeg,JPEG} -type f -size +512k`
 do
 	magick "$file" -strip -resize 2048x2048 -interlace Plane -gaussian-blur 0.05 -quality 85% "$file"
 	magick "$file" -define jpeg:extent=512kb "$file"
